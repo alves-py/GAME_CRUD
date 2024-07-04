7b59f1a68c07b3cb5dbe965475b1e2bb8c48decf
Felipe Alves <felipe.daconceicao@jala.university>
2024-07-03 23:38:12 -0300

    feat[main]: update game Service

--- src/service/serviceGame.js
+++ src/service/serviceGame.js
@@ -1,4 +1,35 @@
 const {
   createGameRepository,
-  uodateGameRepository,
+  updateGameRepository,
+  getGamesRepository,
+  getGameIdRepository,
+  deleteGameRepository,
 } = require("../repository/gameRepository");
+
+const createGameService = (name, description, genre, platform) => {
+  return createGameRepository(name, description, genre, platform);
+};
+
+const updateGameService = (name, description, genre, platform) => {
+  return updateGameRepository(name, description, genre, platform);
+};
+
+const getGamesService = () => {
+  return getGamesRepository();
+};
+
+const getGameIdService = (id) => {
+  return getGameIdRepository(id);
+};
+
+const deleteGameService = (id) => {
+  return deleteGameRepository(id);
+};
+
+module.exports = {
+  createGameService,
+  updateGameService,
+  getGamesService,
+  getGameIdService,
+  deleteGameService,
+};
